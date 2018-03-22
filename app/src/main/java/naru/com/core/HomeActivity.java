1d21ff9ef31696198a702b2df49759a7c54c0b7d
sakib005 <sshadman005@gmail.com>
2018-03-22 19:48:24 +0600

    Core Updated

--- app/src/main/java/naru/com/core/HomeActivity.java
+++ app/src/main/java/naru/com/core/HomeActivity.java
@@ -1,13 +1,47 @@
 package naru.com.core;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView textView;
+    private boolean doubleTap = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (!doubleTap){
+            Toast.makeText(this, "Double tap to exit !", Toast.LENGTH_SHORT).show();
+            doubleTap = true;
+        }else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    finishAffinity();
+
+                }
+            }, 500);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
     }
 }
